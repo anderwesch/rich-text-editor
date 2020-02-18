757eab1a127af7a394c79b84939c5cf4bf645e95
Anderson Weschnhoski <andersonweschnhoski@gmail.com>
2020-02-17 21:34:20 -0300

    Create Format Toolbar compenent

---
+++ src/components/FormatToolbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function FormatToolbar(props) {
+    return(
+        <div className="format-toolbar">
+            {props.children}
+        </div>
+    )
+}
