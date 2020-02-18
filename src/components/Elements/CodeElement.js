5e0f9c35a100c9c89a7978b1198c69b617470c65
Anderson Weschnhoski <andersonweschnhoski@gmail.com>
2020-02-17 22:43:06 -0300

    Removed CodeElement component

--- src/components/Elements/CodeElement.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function CodeElement(props) {
-    return (
-        <pre {...props.attributes}>
-            <code>{props.children}</code>
-        </pre>
-    )
-}
