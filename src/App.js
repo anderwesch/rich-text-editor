c8ff0b3eb48fcd9d79218d433df6df6a5f4e079f
Anderson Weschnhoski <andersonweschnhoski@gmail.com>
2020-02-17 21:20:24 -0300

    Added wrapper class to App

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <TextEditor />
+      <div className="wrapper">
+        <TextEditor />
+      </div>
     </div>
   );
 }
