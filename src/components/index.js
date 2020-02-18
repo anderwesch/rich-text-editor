03886c4662b602b8ddeb7d74005e625e9c4f3e06
Anderson Weschnhoski <andersonweschnhoski@gmail.com>
2020-02-17 22:17:55 -0300

    Added MarkButton component to export

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,7 @@ import CodeElement from './Elements/CodeElement'
 import DefaultElement from './Elements/DefaultElement'
 import KeyHandler from './KeyHandler'
 import FormatToolbar from './FormatToolbar';
-import * as CustomEditor from './CustomEditor'
+import MarkButton from './MarkButton';
+import * as CustomEditor from './CustomEditor';
 
-export { TextEditor, Leaf, CodeElement, DefaultElement, KeyHandler, FormatToolbar, CustomEditor };
+export { TextEditor, Leaf, CodeElement, DefaultElement, KeyHandler, FormatToolbar, CustomEditor, MarkButton };
