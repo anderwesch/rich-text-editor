0f6812d014b599341b7cf33c0a214f2b09278509
Anderson Weschnhoski <andersonweschnhoski@gmail.com>
2020-02-17 23:24:33 -0300

    Updated imports list

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,10 @@
 import TextEditor from './TextEditor';
 import Leaf from './Leaf';
-import CodeElement from './Elements/CodeElement'
 import DefaultElement from './DefaultElement'
 import KeyHandler from './KeyHandler'
 import FormatToolbar from './FormatToolbar';
 import MarkButton from './MarkButton';
+import BlockButton from './BlockButton'
 import * as CustomEditor from './CustomEditor';
 
-export { TextEditor, Leaf, CodeElement, DefaultElement, KeyHandler, FormatToolbar, CustomEditor, MarkButton };
+export { TextEditor, Leaf, DefaultElement, KeyHandler, FormatToolbar, CustomEditor, MarkButton, BlockButton };
